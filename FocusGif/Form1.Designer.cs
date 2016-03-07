e00aad235350901850d36094f827c8c91482e9af
TeamBlueFish <46bluefish@mail.ru>
2016-03-07 17:22:57 +0300

    Init

---
+++ FocusGif/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace FocusGif
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "новый проект";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Chiller", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(246, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(53, 52);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Э";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Image = global::FocusGif.Properties.Resources.w128h1281338911586eraser1;
+            this.button7.Location = new System.Drawing.Point(828, 84);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(36, 36);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Image = global::FocusGif.Properties.Resources.pencil041;
+            this.button6.Location = new System.Drawing.Point(773, 84);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(37, 36);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Image = global::FocusGif.Properties.Resources.paintcan_7788;
+            this.button5.Location = new System.Drawing.Point(882, 84);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(38, 36);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::FocusGif.Properties.Resources.filesave_1;
+            this.button3.Location = new System.Drawing.Point(186, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 52);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::FocusGif.Properties.Resources.inode_directory;
+            this.button2.Location = new System.Drawing.Point(124, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 51);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(44, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(655, 356);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(773, 142);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(37, 39);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(828, 157);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(24, 24);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "1";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(873, 157);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(24, 24);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "2";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(914, 157);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(29, 24);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "3";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 607);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ FocusGif/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FocusGif
+{
+    public partial class Form1 : Form
+    {
+        Color CurrentColor = Color.Black;
+        Color LasticColor = Color.White;
+        Point CurrentPoint;
+        Point PrevPoint;
+        bool isPressed;
+        bool ColPen = false;
+        Graphics g;
+        int SizePen;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isPressed)
+            {
+                if (!ColPen)
+                {
+                    PrevPoint = CurrentPoint;
+                    CurrentPoint = e.Location;
+                    paint();
+                }
+                else
+                {
+                    PrevPoint = CurrentPoint;
+                    CurrentPoint = e.Location;
+                    paintLastic();
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isPressed = true;
+            CurrentPoint = e.Location;
+            if (e.Button == MouseButtons.Right)
+            {
+                ContextMenu m = new ContextMenu();
+                m.MenuItems.Add(0, new MenuItem("Очистить", new EventHandler(RightMouseButton_Click)));
+                m.Show(this, new Point(e.X, e.Y));
+            }
+        }
+
+        private void RightMouseButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+        isPressed = false;
+        }
+
+        private void paint()
+        {
+            Pen p = new Pen(CurrentColor);
+            switch (SizePen)
+            {
+                case 1:
+                    p = new Pen(CurrentColor, 1);
+                    break;
+                case 5:
+                    p = new Pen(CurrentColor, 5);
+                    break;
+                case 10:
+                    p = new Pen(CurrentColor, 10);
+                    break;
+            }
+
+            g.DrawLine(p, PrevPoint, CurrentPoint);
+        }
+
+        private void paintLastic()
+        {
+            Pen pe = new Pen(LasticColor);
+            switch (SizePen)
+            {
+                case 1:
+                    pe = new Pen(LasticColor, 1);
+                    break;
+                case 5:
+                    pe = new Pen(LasticColor, 5);
+                    break;
+                case 10:
+                    pe = new Pen(LasticColor, 10);
+                    break;
+            }
+            g.DrawLine(pe, PrevPoint, CurrentPoint);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ColPen = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ColPen = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            DialogResult D = colorDialog1.ShowDialog();
+            if (D == System.Windows.Forms.DialogResult.OK)
+                CurrentColor = colorDialog1.Color;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            SizePen = 1;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            SizePen = 5;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            SizePen = 10;
+        }
+    }
+
+    
+}
